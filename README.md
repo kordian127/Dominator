86e7deea351cfa67c58bf31ce66bb994258cb1d7
kordian127 <kordian126@gmail.com>
2021-03-11 13:49:28 +0100

    done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Dominator
  
+An array A consisting of N integers is given. The dominator of array A is the value   
+that occurs in more than half of the elements of A.  
+  
+For example, consider array A such that  
+  
+ A[0] = 3    A[1] = 4    A[2] =  3  
+ A[3] = 2    A[4] = 3    A[5] = -1  
+ A[6] = 3    A[7] = 3  
+The dominator of A is 3 because it occurs in 5 out of 8 elements of A   
+(namely in those with indices 0, 2, 4, 6 and 7) and 5 is more than a half of 8.  
---
+++ dominant.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+
+using namespace std;
+
+int main(){
+
+int A[]={-1,4,-1,2,-1,-1,3,-1};
+int sizeA = sizeof(A)/4;
+
+if (sizeA==0)
+    return -1;
+
+map<int,int> m;
+int maxVal=A[0];
+int numb;
+
+for(int i=0;i<sizeA;i++){
+
+    if(m.find(A[i])==m.end())
+        m[A[i]]=1;
+    else
+        m[A[i]]++;
+
+    if (m[A[i]]>maxVal){
+        maxVal=m[A[i]];
+        numb=A[i];
+    }
+}
+if (maxVal<sizeA/2)
+    return -1;
+
+cout<<"Dominant number: "<<numb<<endl<<"Indexes: ";
+for(int j=0;j<sizeA;j++){
+    if(A[j]==numb)
+        cout<<" "<<j<<" ";
+}
+
+return 0;
+}
